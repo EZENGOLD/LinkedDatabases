4a02ff3f72d13b32b0e5ccddf879783af7ff5e07
EZEN <noudekiki@gmail.com>
2020-07-18 10:38:59 +0100

    Perfect money partially finished, moving to test on https

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('', 'PerfectmoneyController@ask_balance')->name('perfect.balance.request');
+Route::post('balance', 'PerfectmoneyController@show_balance')->name('perfect.balance.show');
